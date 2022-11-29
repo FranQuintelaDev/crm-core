203c0b9dd64e9ba5293378f0f4b03d968942ce89
FranQuintelaVela <fraquivel@gmail.com>
2022-11-29 12:09:31 +0100

    Deleted base project Created test and logic

---
+++ src/main/java/com/crm/core/service/UserService.java
@@ -0,0 +1,42 @@
+package com.crm.core.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.crm.core.model.User;
+import com.crm.core.repository.UserRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public User saveUser(User user) {
+        return repository.save(user);
+    }
+
+    public List<User> getUsers() {
+        return repository.getAllUsers();
+    }
+
+    public User getUserById(int id) {
+        return repository.findById(id);
+    }
+
+    public String deleteUser(int id) {
+        repository.delete(id);
+        return "user removed !! " + id;
+    }
+
+    public User updateUser(User user) {
+       return repository.update(user);
+    }
+    
+    public User authUser(User user) {
+    	User search = repository.search(user.getUsername()).stream().filter(x -> x.getPassword().equals(user.getPassword())).findFirst().get();
+		return search;
+    }
+}
--- src/test/java/com/crm/core/CrmCoreApplicationTests.java
+++ src/test/java/com/crm/core/CrmCoreApplicationTests.java
@@ -3,34 +3,127 @@ package com.crm.core;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.crm.core.model.Product;
-import com.crm.core.repository.ProductRepository;
-import com.crm.core.service.ProductService;
-
+import com.crm.core.model.User;
+import com.crm.core.service.UserService;
 
 @SpringBootTest
 class CrmCoreApplicationTests {
 
+
 	@Autowired
-	private ProductService productService;
+	private UserService userService;
 	
 	@BeforeEach
 	void setUp() {
 	}
-	
+
+	@Disabled("Sample test")
 	@Test
 	void contextLoads() {
 	}
 
-
+	@Disabled("To be implemented")
 	@Test
-	public void saveProductOKExpectsOK() {
-		Product expectedProduct = new Product(1, "product 1", 10, 10.0);
-		Product actualProduct = productService.saveProduct(expectedProduct);
-		assertEquals(expectedProduct.getId(),actualProduct.getId());
+	public void signUpOKExpectOK() {
+		User expectedUser = new User(1, "Ford employee", "samplepass");
+		User actualUser = userService.authUser(expectedUser);
+		assertEquals(expectedUser.getId(), actualUser.getId());
 	}
+	
+	@Disabled("To be implemented")
+	@Test
+	public void signUpExistingUserExpectKO() {
+	}
+	
+	@Disabled("To be implemented")
+	@Test
+	public void signUpInvalidDataExpectKO() {
+	}
+	
+	@Disabled("To be implemented")
+	@Test
+	public void loginOKExpectOK() {
+	}
+	
+	@Disabled("To be implemented")
+	@Test
+	public void loginWrongPassExpectKO() {
+	}
+	
+	@Disabled("To be implemented")
+	@Test
+	public void loginNonExistingUserExpectKO() {
+	}
+	
+	@Disabled("To be implemented")
+	@Test
+	public void loginInvalidDataExpectKO() {
+	}
+	
+	@Disabled("To be implemented")
+	@Test
+	public void getOpportunitiesThenFindExistingOpExpectOK() {
+	}
+	
+	@Disabled("To be implemented")
+	@Test
+	public void getOpportunitiesAsFailUserExpectKO() {
+	}
+	
+	@Disabled("To be implemented")
+	@Test
+	public void getOpportunitiesThenCantFindExistingClientExpectOK() {
+	}
+	
+	@Disabled("To be implemented")
+	@Test
+	public void getOpportunityExpectOK() {
+	}
+	
+	@Disabled("To be implemented")
+	@Test
+	public void getOpportunityWithContactsExpectOK() {
+	}
+	
+	@Disabled("To be implemented")
+	@Test
+	public void getOpportunityWithNonExistingOpExpectKO() {
+	}
+	
+	@Disabled("To be implemented")
+	@Test
+	public void markAsClientOKExpectOK() {
+	}
+	
+	@Disabled("To be implemented")
+	@Test
+	public void markAsClientWithInvalidDataExpectKO() {
+	}
+	
+	@Disabled("To be implemented")
+	@Test
+	public void getClientsThenFindExistingClExpectOK() {
+	}
+	
+	@Disabled("To be implemented")
+	@Test
+	public void getClientsThenCantFindExistingNonClientExpectOK() {
+	}
+	
+	@Disabled("To be implemented")
+	@Test
+	public void saveContactExpectOK() {
+	}
+
+	@Disabled("To be implemented")
+	@Test
+	public void saveContactWithInvalidDataExpectKO() {
+	}
+	
+
 }
